eb988ab8f2cc02919e3a4884152582f92706025c
Nakk1makkara <111732406+Nakk1makkara@users.noreply.github.com>
2023-11-22 22:02:32 +0200

    shooting effects

--- GigaMarineMayhem/Assets/Scripts/NewScripts/PlayerBehaviour/Shooting.cs
+++ GigaMarineMayhem/Assets/Scripts/NewScripts/PlayerBehaviour/Shooting.cs
@@ -18,6 +18,9 @@ public class Shooting : MonoBehaviour
 
     public TextMeshProUGUI ammoText; // Reference to the TextMeshProUGUI component on your HUD
 
+    public AudioSource shootingAudioSource; // Reference to the AudioSource component for shooting sound
+    public AudioSource reloadAudioSource;   // Reference to the AudioSource component for reload sound
+
     void Start()
     {
         currentAmmo = maxAmmo;
@@ -36,11 +39,17 @@ public class Shooting : MonoBehaviour
     {
         isShooting = true;
 
+        // Play shooting sound continuously while shooting
+        shootingAudioSource.Play();
+
         while (Input.GetButton("Fire1") && currentAmmo > 0)
         {
             Shoot();
             yield return new WaitForSeconds(shootingInterval);
         }
+
+        // Stop shooting sound
+        shootingAudioSource.Stop();
 
         if (currentAmmo <= 0)
         {
@@ -54,6 +63,10 @@ public class Shooting : MonoBehaviour
     {
         isReloading = true;
         ammoText.text = "Reloading...";
+
+        // Play reload sound
+        reloadAudioSource.Play();
+
         yield return new WaitForSeconds(reloadTime);
         currentAmmo = maxAmmo;
         isReloading = false;
