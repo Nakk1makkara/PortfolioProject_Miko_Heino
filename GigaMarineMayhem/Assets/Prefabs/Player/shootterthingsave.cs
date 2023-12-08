bf1596cc0bf23faf742f95f2f1c9ee34da87ae20
Nakk1makkara <111732406+Nakk1makkara@users.noreply.github.com>
2023-12-08 19:02:54 +0200

    shotgun added

---
+++ GigaMarineMayhem/Assets/Prefabs/Player/shootterthingsave.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class shootterthingsave : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float bulletForce = 20f;
+    public float shootingInterval = 0.5f;
+    public float reloadTime = 4f;
+
+    private bool isShooting = false;
+    private bool isReloading = false;
+
+    public TextMeshProUGUI ammoText; 
+
+    public AudioSource shootingAudioSource; 
+    public AudioSource reloadAudioSource;  
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+        UpdateAmmoText();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !isShooting && !isReloading)
+        {
+            StartCoroutine(ShootRoutine());
+        }
+    }
+
+    IEnumerator ShootRoutine()
+    {
+        isShooting = true;
+
+        // Play shooting sound continuously while shooting
+        shootingAudioSource.Play();
+
+        while (Input.GetButton("Fire1") && currentAmmo > 0)
+        {
+            Shoot();
+            yield return new WaitForSeconds(shootingInterval);
+        }
+
+        // Stop shooting sound
+        shootingAudioSource.Stop();
+
+        if (currentAmmo <= 0)
+        {
+            yield return StartCoroutine(Reload());
+        }
+
+        isShooting = false;
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        ammoText.text = "Reloading...";
+
+        // Play reload sound
+        reloadAudioSource.Play();
+
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+        UpdateAmmoText();
+    }
+
+    void Shoot()
+    {
+        currentAmmo--;
+        UpdateAmmoText();
+
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+    void UpdateAmmoText()
+    {
+        ammoText.text = "Ammo: " + currentAmmo.ToString();
+    }
+}
--- GigaMarineMayhem/Assets/Scripts/NewScripts/PlayerBehaviour/Shooting.cs
+++ GigaMarineMayhem/Assets/Scripts/NewScripts/PlayerBehaviour/Shooting.cs
@@ -4,52 +4,116 @@ using TMPro;
 
 public class Shooting : MonoBehaviour
 {
-    public Transform firePoint;
-    public GameObject bulletPrefab;
+    [System.Serializable]
+    public class Weapon
+    {
+        public Transform firePoint;
+        public GameObject bulletPrefab;
+        public int maxAmmo = 10;
+        public float bulletForce = 20f;
+        public float shootingInterval = 0.5f;
+        public float reloadTime = 4f;
+        public AudioSource shootingAudioSource;
+        public AudioSource reloadAudioSource;
+    }
 
-    public int maxAmmo = 10;
+    public Weapon[] weapons;
+    private int currentWeaponIndex = 0;
+
     private int currentAmmo;
-    public float bulletForce = 20f;
-    public float shootingInterval = 0.5f;
-    public float reloadTime = 4f;
-
     private bool isShooting = false;
     private bool isReloading = false;
 
-    public TextMeshProUGUI ammoText; 
+    private bool isShootingRoutineActive = false; // Add this flag
 
-    public AudioSource shootingAudioSource; 
-    public AudioSource reloadAudioSource;  
+    public TextMeshProUGUI ammoText;
 
     void Start()
     {
-        currentAmmo = maxAmmo;
-        UpdateAmmoText();
+        SwitchWeapon(currentWeaponIndex);
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // LMG
+        {
+            SwitchWeapon(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) // Shotgun
+        {
+            SwitchWeapon(1);
+        }
+        // Add more conditions for other weapon keys if needed
+
         if (Input.GetButtonDown("Fire1") && !isShooting && !isReloading)
         {
-            StartCoroutine(ShootRoutine());
+            if (currentWeaponIndex == 0) // LMG
+            {
+                isShooting = true;
+                StartCoroutine(ContinuousLMGFire());
+            }
+            else if (currentWeaponIndex == 1 && !isShootingRoutineActive) // Shotgun
+            {
+                isShooting = true;
+                StartCoroutine(ShootRoutine());
+            }
         }
+        else if (Input.GetButtonUp("Fire1") && currentWeaponIndex == 0) // Stop LMG fire on button release
+        {
+            isShooting = false;
+        }
+    }
+
+    void SwitchWeapon(int index)
+    {
+        currentWeaponIndex = Mathf.Clamp(index, 0, weapons.Length - 1);
+        currentAmmo = weapons[currentWeaponIndex].maxAmmo;
+        UpdateAmmoText();
     }
 
     IEnumerator ShootRoutine()
     {
-        isShooting = true;
+        // Set the flag to indicate the routine is active
+        isShootingRoutineActive = true;
 
+        // Debug statement
+        Debug.Log("Playing Shooting Sound");
+
+        // Play shooting sound once
+        weapons[currentWeaponIndex].shootingAudioSource.Play();
+
+        // Fire a single bullet for the shotgun
+        Shoot();
+
+
+        if (currentAmmo <= 0)
+        {
+            yield return StartCoroutine(Reload());
+        }
+
+        // Reset the flag when the routine is finished
+        isShootingRoutineActive = false;
+        isShooting = false; // Reset the isShooting flag
+    }
+
+
+
+
+
+
+    IEnumerator ContinuousLMGFire()
+    {
         // Play shooting sound continuously while shooting
-        shootingAudioSource.Play();
+        weapons[currentWeaponIndex].shootingAudioSource.Play();
 
         while (Input.GetButton("Fire1") && currentAmmo > 0)
         {
             Shoot();
-            yield return new WaitForSeconds(shootingInterval);
+            yield return new WaitForSeconds(weapons[currentWeaponIndex].shootingInterval);
         }
 
         // Stop shooting sound
-        shootingAudioSource.Stop();
+        weapons[currentWeaponIndex].shootingAudioSource.Stop();
 
         if (currentAmmo <= 0)
         {
@@ -65,10 +129,10 @@ public class Shooting : MonoBehaviour
         ammoText.text = "Reloading...";
 
         // Play reload sound
-        reloadAudioSource.Play();
+        weapons[currentWeaponIndex].reloadAudioSource.Play();
 
-        yield return new WaitForSeconds(reloadTime);
-        currentAmmo = maxAmmo;
+        yield return new WaitForSeconds(weapons[currentWeaponIndex].reloadTime);
+        currentAmmo = weapons[currentWeaponIndex].maxAmmo;
         isReloading = false;
         UpdateAmmoText();
     }
@@ -78,13 +142,31 @@ public class Shooting : MonoBehaviour
         currentAmmo--;
         UpdateAmmoText();
 
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        if (currentWeaponIndex == 1) // Shotgun
+        {
+            for (int i = 0; i < 10; i++) // Adjust the number of bullets in the cone as needed
+            {
+                float angle = Random.Range(-15f, 15f); // Adjust the spread angle as needed
+                Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+                Vector3 bulletDirection = rotation * weapons[currentWeaponIndex].firePoint.up;
+
+                GameObject bullet = Instantiate(weapons[currentWeaponIndex].bulletPrefab, weapons[currentWeaponIndex].firePoint.position, rotation);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(bulletDirection * weapons[currentWeaponIndex].bulletForce, ForceMode2D.Impulse);
+            }
+        }
+        else // Other weapons (e.g., LMG)
+        {
+            // Only instantiate one bullet for other weapons
+            GameObject bullet = Instantiate(weapons[currentWeaponIndex].bulletPrefab, weapons[currentWeaponIndex].firePoint.position, weapons[currentWeaponIndex].firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(weapons[currentWeaponIndex].firePoint.up * weapons[currentWeaponIndex].bulletForce, ForceMode2D.Impulse);
+        }
     }
 
     void UpdateAmmoText()
     {
         ammoText.text = "Ammo: " + currentAmmo.ToString();
     }
+    
 }
